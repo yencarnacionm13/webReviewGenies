0a2603bb77e180589d148496b76fb4c42055b840
yencarnacionm13 <122704639+yencarnacionm13@users.noreply.github.com>
2023-07-28 10:37:41 -0400

    Cambios: conexion a base de datos en azure

--- config/db.config.js
+++ config/db.config.js
@@ -17,12 +17,31 @@
   }
 };*/
 
- module.exports = {
+/*  module.exports = {
   HOST: "localhost",
   PORT: "1433",
   USER: "userDATI",
   PASSWORD: "daTi2@23?-",
   DB: "Dati",
+  dialect: "mssql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: false // change to true for local dev / self-signed certs
+  }
+};  */
+
+module.exports = {
+  HOST: "reviewgenies.database.windows.net",
+  PORT: "1433",
+  USER: "fullpower",
+  PASSWORD: "Rai@2022",
+  DB: "reviewgenie_app",
   dialect: "mssql",
   pool: {
     max: 5,
