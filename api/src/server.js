eac156f047ef01acf9d518c3123354558a522aec
yencarnacionm13 <122704639+yencarnacionm13@users.noreply.github.com>
2023-07-31 13:44:16 -0400

    Divison de la api y el front para la publicacion

--- api/src/server.js
+++ api/src/server.js
@@ -44,7 +44,7 @@ app.use(passport.session())
 
 // Prepare server for Bootstrap, jQuery and PowerBI files
 /* app.use('/js', express.static('./node_modules/bootstrap/dist/js/')); // Redirect bootstrap JS */
-app.use('/css', express.static('/node_modules/bootstrap/dist/css'))
+/* app.use('/css', express.static('/node_modules/bootstrap/dist/css'))
 app.use('/js', express.static('/node_modules/bootstrap/dist/js'))
 app.use('/js', express.static('/node_modules/jquery/dist'))
 
@@ -53,7 +53,23 @@ app.use('/js', express.static('./node_modules/powerbi-client/dist/')) // Redirec
 app.use('/js', express.static('./node_modules/powerbi-report-authoring/dist/')) // Redirect JS PowerBIAuthoring
 app.use('/css', express.static('./node_modules/bootstrap/dist/css/')); // Redirect CSS bootstrap
 app.use('/public', express.static('./public/')); // Use custom JS and CSS files
+ */
 
+
+app.use('/js', express.static('/node_modules/bootstrap/dist/js'))
+app.use('/js', express.static('/node_modules/jquery/dist'))
+
+app.use('/js', express.static('/node_modules/jquery/dist/')); // Redirect JS jQuery
+app.use('/js', express.static('/node_modules/powerbi-client/dist/')) // Redirect JS PowerBI
+app.use('/js', express.static('/node_modules/powerbi-report-authoring/dist/')) // Redirect JS PowerBIAuthoring
+//app.use('/css', express.static('/node_modules/bootstrap/dist/css/')); // Redirect CSS bootstrap
+/* app.use('/css', express.static('/node_modules/bootstrap/dist/css')) */
+
+app.use('/public', express.static(process.cwd() +'/../public')); // Use custom JS and CSS files
+app.use('/images', express.static(process.cwd() +'/../public/images')); // Use custom JS and CSS files
+app.use('/vendors', express.static(process.cwd() +'/../public/vendors'));
+app.use('/css', express.static(process.cwd() +'/../public/css'));
+app.use('/js', express.static(process.cwd() +'/../public/js'));
 
 require('../routes/auth.routes')(app);
 
@@ -63,46 +79,31 @@ require('../routes/filters.routes')(app);
 
 require('../routes/params.routes')(app);
 
+
+
 app.get('/', checkNotAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/login.html'));
+    res.sendFile(path.join(process.cwd() + '/../public/views/login.html'));
 });
 
 app.get('/login', checkNotAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/login.html'));
+    res.sendFile(path.join(process.cwd() + '/../public/views/login.html'));
 });
 
 app.get('/register', checkNotAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/register.html'));
+    res.sendFile(path.join(process.cwd() + '/../public/views/register.html'));
 });
 
 app.get('/recover', checkNotAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/recover.html'));
+    res.sendFile(path.join(process.cwd() + '/../public/views/recover.html'));
 });
 
 app.get('/password', checkAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/password.html'));
+    res.sendFile(path.join(process.cwd() + '/../public/views/password.html'));
 });
 
 app.get('/dashboard', checkAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/index.html'));
+    res.sendFile(path.join(process.cwd() + '/../public/views/index.html'));
 });
-
-/* app.get('/serietiempo', checkAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/serietiempo.html'));
-});
-
-app.get('/busquedapropiedad', checkAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/busquedapropiedad.html'));
-});
-
-app.get('/factorrentaventa', checkAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/factorrentaventa.html'));
-});
-
-app.get('/sectores', checkAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname + '/../views/sectores.html'));
-}); */
-
 
 
 app.get('/getEmbedToken', checkAuthenticated, async function (req, res) {
@@ -151,57 +152,6 @@ const port = process.env.PORT || 5300;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-
-
-
-
-/* const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
-
-const initializePassport = require('../passport-config')
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
- */
-
-
-/* app.use(flash()) */
-/* app.use(session({
-    secret: process.env.SESSION_SECRET || 'Test',
-    resave: false,
-    saveUninitialized: false
-})) */
-/* app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method')) */
-
-
-
-/* app.post('/', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/',
-    failureFlash: true
-})) */
-
-
-/* function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-
-    res.redirect('/')
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/dashboard')
-    }
-    next()
-} */
 
 
 
